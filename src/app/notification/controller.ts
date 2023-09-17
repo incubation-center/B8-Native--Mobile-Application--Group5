a1a83150de0671dae32dd4f0f39cbd7e534696b8
Hakly Long <hakly.l>
2023-09-17 23:23:51 +0700

    get all notification and notification by id

--- src/app/notification/controller.ts
+++ src/app/notification/controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import NotificationModel from './model';
+import PropertyModel from '../property/models';
 import { JwtPayload } from 'jsonwebtoken';
 interface AuthenticatedRequest extends Request {
     user?: JwtPayload;
@@ -7,8 +8,23 @@ interface AuthenticatedRequest extends Request {
 export const getAllNotifications = async (req: AuthenticatedRequest, res: Response) => {
     try {
         const notifications = await NotificationModel.findAll({where : {userId: req.user?.id}});
+        for(let noti of notifications){
+            const property = await PropertyModel.findOne({where: {id: noti.propertyId}});
+            noti.dataValues.image = property?.image;
+        }
         res.json(notifications);
     } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+export const getNotificationByPropertyId = async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const { id } = req.params;
+        const notifications = await NotificationModel.findOne({where : {id: id}});
+        res.json(notifications);
+    }
+    catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
--- src/app/notification/routes.ts
+++ src/app/notification/routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { authenticateToken } from "../../helper/authenticationToken";
 import {
-    getAllNotifications
+    getAllNotifications,
+    getNotificationByPropertyId
 } from "./controller";
 
 const router = express.Router();
 
 router.get("/", authenticateToken, getAllNotifications);
+router.get("/:id", authenticateToken, getNotificationByPropertyId);
 export default router;
